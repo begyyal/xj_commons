e1e474d72b8ef2d43dff87010eab796b8b859586
begyyal <ololo@ololo.ll>
2021-05-16 02:57:24 +0900

    simplifyǉ

--- src/begyyal/commons/util/math/SuperMath.java
+++ src/begyyal/commons/util/math/SuperMath.java
@@ -9,4 +9,8 @@ public class SuperMath {
     public static int lcm(int a, int b) {
 	return a * b / gcd(a, b);
     }
+
+    public static int simplify(int x) {
+	return x > 0 ? 1 : x < 0 ? -1 : 0;
+    }
 }
