c2cdf722192152faf430c09b6cfad6f4ace48225
begyyal <begyyal@gmail.com>
2022-02-24 04:17:39 +0900

    キャッシュマップ修正

--- src/begyyal/commons/util/cache/SimpleCacheResolver.java
+++ src/begyyal/commons/util/cache/SimpleCacheResolver.java
@@ -7,23 +7,76 @@ import com.google.common.collect.Maps;
 
 public class SimpleCacheResolver {
 
-    private static final Map<Class<?>, Map<?, ?>> cache = Maps.newConcurrentMap();
+    // key型、KVマップ
+    private static final Map<Class<?>, Map<?, ?>> //
+    publicCache = Maps.newConcurrentMap();
+    // 呼び出し元クラス、key型、KVマップ
+    private static final Map<Class<?>, Map<Class<?>, Map<?, ?>>> //
+    privateCache = Maps.newConcurrentMap();
 
-    public static <K, V> V get(K key, Supplier<V> s) {
+    public static <K, V> V getAsPublic(K key, Supplier<? extends V> s) {
 	@SuppressWarnings("unchecked")
-	var miniCache = (Map<K, V>) cache.computeIfAbsent(key.getClass(), k -> {
-	    var m = Maps.<K, V>newConcurrentMap();
-	    m.put(key, s.get());
-	    return m;
-	});
-	return miniCache.computeIfAbsent(key, k -> s.get());
+	var cache = (Map<K, V>) publicCache.computeIfAbsent(
+	    key.getClass(), k -> Maps.<K, V>newConcurrentMap());
+	return cache.computeIfAbsent(key, k -> s.get());
     }
 
-    public static void remove(Object key) {
-	cache.remove(key.getClass());
+    public static <K, V> V getAsPrivate(K key, Supplier<? extends V> s) {
+	return getAsPrivate(getCaller(), key, s);
     }
 
-    public static void clear() {
-	cache.clear();
+    public static <K, V> V getAsPrivate(Class<?> caller, K key, Supplier<? extends V> s) {
+	@SuppressWarnings("unchecked")
+	var small = (Map<K, V>) privateCache
+	    .computeIfAbsent(caller, k -> Maps.newConcurrentMap())
+	    .computeIfAbsent(key.getClass(), k -> Maps.<K, V>newConcurrentMap());
+	return small.computeIfAbsent(key, k -> s.get());
+    }
+
+    public static void removeAsPublic(Object key) {
+	var m = publicCache.get(key.getClass());
+	if (m != null)
+	    m.remove(key);
+    }
+
+    public static void removeAsPrivate(Object key) {
+	removeAsPrivate(getCaller(), key);
+    }
+
+    public static void removeAsPrivate(Class<?> caller, Object key) {
+	Map<?, Map<?, ?>> m = privateCache.get(caller);
+	Map<?, ?> s = null;
+	if (m != null && (s = m.get(key.getClass())) != null)
+	    s.remove(key);
+    }
+
+    public static void clearAsPublic(Class<?> key) {
+	publicCache.remove(key);
+    }
+
+    public static void clearAsPrivate(Class<?> key) {
+	clearAsPrivate(getCaller(), key);
+    }
+
+    public static void clearAsPrivate(Class<?> caller, Class<?> key) {
+	Map<?, Map<?, ?>> m = privateCache.get(caller);
+	if (m != null && m.containsKey(key))
+	    m.remove(key);
+    }
+
+    private static Class<?> getCaller() {
+	Class<?> caller;
+	try {
+	    caller = Class.forName(Thread.currentThread().getStackTrace()[3].getClassName());
+	} catch (ClassNotFoundException e) {
+	    throw new RuntimeException(
+		"Caller class is not found, it may need to input the class by overload method.");
+	}
+	return caller;
+    }
+
+    public static void clearAll() {
+	publicCache.clear();
+	privateCache.clear();
     }
 }
--- src/begyyal/commons/util/function/ReflectionResolver.java
+++ src/begyyal/commons/util/function/ReflectionResolver.java
@@ -16,153 +16,153 @@ import begyyal.commons.util.object.SuperList.SuperListGen;
 public class ReflectionResolver {
 
     public static void doMethodThroughAll(
-            Class<?> clazz,
-            String methodName,
-            Object instance,
-            Object... args) {
-        try {
-            doMethod(clazz, methodName, instance, args);
-        } catch (Exception e) {
-        }
+	Class<?> clazz,
+	String methodName,
+	Object instance,
+	Object... args) {
+	try {
+	    doMethod(clazz, methodName, instance, args);
+	} catch (Exception e) {
+	}
     }
 
     public static <V> V doMethodThroughAll(
-            Class<?> clazz,
-            Class<V> rtnClazz,
-            String methodName,
-            Object instance,
-            Object... args) {
-        try {
-            return doMethod(clazz, rtnClazz, methodName, instance, args);
-        } catch (Exception e) {
-            return null;
-        }
+	Class<?> clazz,
+	Class<V> rtnClazz,
+	String methodName,
+	Object instance,
+	Object... args) {
+	try {
+	    return doMethod(clazz, rtnClazz, methodName, instance, args);
+	} catch (Exception e) {
+	    return null;
+	}
     }
 
     public static void doMethod(
-            Class<?> clazz,
-            String methodName,
-            Object instance,
-            Object... args) throws Exception {
-        doMethod(clazz, null, methodName, instance, args);
+	Class<?> clazz,
+	String methodName,
+	Object instance,
+	Object... args) throws Exception {
+	doMethod(clazz, null, methodName, instance, args);
     }
 
     public static <V> V doMethod(
-            Class<?> clazz,
-            Class<V> rtnClazz,
-            String methodName,
-            Object instance,
-            Object... args) throws Exception {
+	Class<?> clazz,
+	Class<V> rtnClazz,
+	String methodName,
+	Object instance,
+	Object... args) throws Exception {
 
-        V result = null;
-        try {
-            Class<?>[] argsClazz = Arrays.stream(args)
-                    .map(o -> o.getClass())
-                    .collect(Collectors.toList())
-                    .toArray(new Class[] {});
-            result = doMethodCore(clazz, rtnClazz, methodName, instance, argsClazz, args);
+	V result = null;
+	try {
+	    Class<?>[] argsClazz = Arrays.stream(args)
+		.map(o -> o.getClass())
+		.collect(Collectors.toList())
+		.toArray(new Class[] {});
+	    result = doMethodCore(clazz, rtnClazz, methodName, instance, argsClazz, args);
 
-        } catch (NoSuchMethodException e) {
-            // invokeで実行する際に求められる引数型の完全一致に対する特殊考慮
-            result = cast(rtnClazz, resolveArgsClass(clazz, rtnClazz, methodName, instance, args));
+	} catch (NoSuchMethodException e) {
+	    // invokeで実行する際に求められる引数型の完全一致に対する特殊考慮
+	    result = cast(rtnClazz, resolveArgsClass(clazz, rtnClazz, methodName, instance, args));
 
-        } catch (Exception e) {
-            throw e;
-        }
+	} catch (Exception e) {
+	    throw e;
+	}
 
-        return result;
+	return result;
     }
 
     private static Object resolveArgsClass(
-            Class<?> clazz,
-            Class<?> rtnClazz,
-            String methodName,
-            Object instance,
-            Object[] args) throws Exception {
+	Class<?> clazz,
+	Class<?> rtnClazz,
+	String methodName,
+	Object instance,
+	Object[] args) throws Exception {
 
-        List<Set<Class<?>>> argsClazzCandidates = //
-                Arrays.stream(args)
-                        .map(a -> fillClassExpression(a.getClass(), Sets.newHashSet()))
-                        .collect(Collectors.toList());
+	List<Set<Class<?>>> argsClazzCandidates = //
+		Arrays.stream(args)
+		    .map(a -> getClassExpression(a.getClass()))
+		    .collect(Collectors.toList());
 
-        List<Iterator<Class<?>>> argsClazzIterators = //
-                argsClazzCandidates.stream()
-                        .map(set -> set.iterator())
-                        .collect(Collectors.toList());
+	List<Iterator<Class<?>>> argsClazzIterators = //
+		argsClazzCandidates.stream()
+		    .map(set -> set.iterator())
+		    .collect(Collectors.toList());
 
-        Class<?>[] argsClazz = Arrays.stream(args)//
-                .map(o -> o.getClass())
-                .collect(Collectors.toList())
-                .toArray(new Class[] {});
+	Class<?>[] argsClazz = Arrays.stream(args)//
+	    .map(o -> o.getClass())
+	    .collect(Collectors.toList())
+	    .toArray(new Class[] {});
 
-        while (argsClazzIterators.stream().anyMatch(ite -> ite.hasNext())) {
+	while (argsClazzIterators.stream().anyMatch(ite -> ite.hasNext())) {
 
-            boolean done = false;
-            int index = 0;
-            for (Iterator<Class<?>> ite : argsClazzIterators) {
-                if (!done && ite.hasNext()) {
-                    argsClazz[index] = ite.next();
-                    IntStream.range(0, index).forEach(
-                            i -> argsClazzIterators.set(i, argsClazzCandidates.get(i).iterator()));
-                    done = true;
-                }
-                index++;
-            }
+	    boolean done = false;
+	    int index = 0;
+	    for (Iterator<Class<?>> ite : argsClazzIterators) {
+		if (!done && ite.hasNext()) {
+		    argsClazz[index] = ite.next();
+		    IntStream.range(0, index).forEach(
+			i -> argsClazzIterators.set(i, argsClazzCandidates.get(i).iterator()));
+		    done = true;
+		}
+		index++;
+	    }
 
-            try {
-                return doMethodCore(clazz, rtnClazz, methodName, instance, argsClazz, args);
-            } catch (NoSuchMethodException e) {
-            } catch (Exception e) {
-                throw e;
-            }
-        }
+	    try {
+		return doMethodCore(clazz, rtnClazz, methodName, instance, argsClazz, args);
+	    } catch (NoSuchMethodException e) {
+	    } catch (Exception e) {
+		throw e;
+	    }
+	}
 
-        throw new NoSuchMethodException();
+	throw new NoSuchMethodException();
     }
 
-    private static Set<Class<?>>
-            fillClassExpression(Class<?> clazz, Set<Class<?>> classExpression) {
+    public static Set<Class<?>> getClassExpression(Class<?> clazz) {
+	var set = Sets.<Class<?>>newHashSet();
+	fillClassExpression(clazz, set);
+	return set;
+    }
 
-        classExpression.add(clazz);
-
-        if (clazz.getSuperclass() != null)
-            fillClassExpression(clazz.getSuperclass(), classExpression);
-
-        if (clazz.getInterfaces() != null)
-            Arrays.stream(clazz.getInterfaces())
-                    .forEach(i -> fillClassExpression(i, classExpression));
-
-        return classExpression;
+    private static void fillClassExpression(Class<?> clazz, Set<Class<?>> classExpression) {
+	classExpression.add(clazz);
+	if (clazz.getSuperclass() != null)
+	    fillClassExpression(clazz.getSuperclass(), classExpression);
+	if (clazz.getInterfaces() != null)
+	    Arrays.stream(clazz.getInterfaces())
+		.forEach(i -> fillClassExpression(i, classExpression));
     }
 
     private static <V> V doMethodCore(
-            Class<?> clazz,
-            Class<V> rtnClazz,
-            String methodName,
-            Object instance,
-            Class<?>[] argsClazz,
-            Object[] args) throws Exception {
-        return cast(rtnClazz,
-                clazz.getMethod(methodName, argsClazz).invoke(instance, args));
+	Class<?> clazz,
+	Class<V> rtnClazz,
+	String methodName,
+	Object instance,
+	Class<?>[] argsClazz,
+	Object[] args) throws Exception {
+	return cast(rtnClazz,
+	    clazz.getMethod(methodName, argsClazz).invoke(instance, args));
     }
 
     public static <V> V cast(Class<V> clazz, Object o) {
-        return o != null && clazz != null && clazz.isInstance(o) ? clazz.cast(o) : null;
+	return o != null && clazz != null && clazz.isInstance(o) ? clazz.cast(o) : null;
     }
 
     public static <V> SuperList<V> cast(Class<V> clazz, List<?> col) {
-        return CollectionUtils.isNotEmpty(col) && clazz != null
-                ? col.stream()
-                        .filter(f -> clazz.isInstance(f))
-                        .map(f -> clazz.cast(f))
-                        .collect(SuperListGen.collect())
-                : SuperListGen.empty();
+	return CollectionUtils.isNotEmpty(col) && clazz != null
+		? col.stream()
+		    .filter(f -> clazz.isInstance(f))
+		    .map(f -> clazz.cast(f))
+		    .collect(SuperListGen.collect())
+		: SuperListGen.empty();
     }
 
     public static <V> Stream<V> cast(Class<V> clazz, Stream<?> s) {
-        return s != null && clazz != null
-                ? s.filter(f -> clazz.isInstance(f))
-                        .map(f -> clazz.cast(f))
-                : Stream.empty();
+	return s != null && clazz != null
+		? s.filter(f -> clazz.isInstance(f))
+		    .map(f -> clazz.cast(f))
+		: Stream.empty();
     }
 }
