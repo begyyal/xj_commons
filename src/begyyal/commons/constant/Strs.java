5345e480fad09c41f2a1bf98daba615d6e517533
begyyal <begyyal@gmail.com>
2022-03-22 05:15:09 +0900

    汎用文字拡充

--- src/begyyal/commons/constant/Strs.java
+++ src/begyyal/commons/constant/Strs.java
@@ -3,11 +3,14 @@ package begyyal.commons.constant;
 public final class Strs {
     public static final String empty = "";
     public static final String space = " ";
+    public static final String hyphen = "-";
+    public static final String underScore = "_";
     public static final String comma = ",";
     public static final String colon = ":";
     public static final String semiColon = ";";
     public static final String equal = "=";
     public static final String slash = "/";
+    public static final String backSlash = "\\";
     public static final String quotation = "¥'";
     public static final String dblQuotation = "\"";
     public static final String bracket1start = "[";
