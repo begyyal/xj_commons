5371d12195ee5ce7e8d9d9876a9fd90fdc07e617
begyyal <begyyal@gmail.com>
2022-03-23 14:14:28 +0900

    モジュール化

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module begyyal.xj_commons {
+    exports begyyal.commons.constant;
+    exports begyyal.commons.constant.web;
+    exports begyyal.commons.object;
+    exports begyyal.commons.object.collection;
+    exports begyyal.commons.util.cache;
+    exports begyyal.commons.util.function;
+}
